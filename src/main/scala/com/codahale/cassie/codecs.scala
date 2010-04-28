6244c835921eda4228cbccdb22baa9a9ca74d78f
Coda Hale <coda.hale@gmail.com>
2010-04-28 09:45:28 -0700

    Added implicit conversions for Cassie's types.

--- src/main/scala/com/codahale/cassie/codecs.scala
+++ src/main/scala/com/codahale/cassie/codecs.scala
@@ -3,16 +3,27 @@ package com.codahale.cassie
 import codecs._
 
 /**
- * A collection of codecs for all the types supported by Cassie.
+ * Implicit parameters and conversions for all the types supported by Cassie.
  *
  * @author coda
  */
 package object codecs {
   implicit val utf8 = Utf8Codec
+
   implicit val ascii = AsciiCodec
+  implicit def String2AsciiString(value: String): AsciiString = AsciiString(value)
+
   implicit val bytes = ByteArrayCodec
+
   implicit val fixedInt = FixedIntCodec
+  implicit def Int2FixedInt(value: Int): FixedInt = FixedInt(value)
+
   implicit val fixedLong = FixedLongCodec
+  implicit def Long2FixedLong(value: Long): FixedLong = FixedLong(value)
+
   implicit val varInt = VarIntCodec
+  implicit def Int2FVarInt(value: Int): VarInt = VarInt(value)
+
   implicit val varLong = VarLongCodec
+  implicit def Long2VarLong(value: Long): VarLong = VarLong(value)
 }
