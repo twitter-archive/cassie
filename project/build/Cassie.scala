349a66ab756e6d8a68adf6fe0e1ac7514b95f107
Coda Hale <coda.hale@gmail.com>
2010-05-11 11:13:51 -0700

    Still waiting on scalaj.

--- project/build/Cassie.scala
+++ project/build/Cassie.scala
@@ -19,7 +19,11 @@ class Cassie(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) {
   /**
    * Test Dependencies
    */
-  val scalaTest = "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-with-test-interfaces-0.3-SNAPSHOT" % "test" withSources() intransitive()
+  val scalaTest = buildScalaVersion match {
+    case "2.8.0.Beta1" => "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-with-test-interfaces-0.3-SNAPSHOT" % "test" withSources() intransitive()
+    case "2.8.0.RC2" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC2-SNAPSHOT" % "test" withSources() intransitive()
+    case unknown => error("no known scalatest impl for %s".format(unknown))
+  }
   val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test" withSources()
 
   /**
