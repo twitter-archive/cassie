66b16ade35bfaf4ed74ca324b1cfb1e5e5841da7
Coda Hale <coda.hale@gmail.com>
2010-06-02 11:36:25 -0700

    Add RC3 as a build target and use my rsync-sbt plugin.

--- project/build/Cassie.scala
+++ project/build/Cassie.scala
@@ -1,4 +1,4 @@
-class Cassie(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) {
+class Cassie(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) with rsync.RsyncPublishing {
   /**
    * Repositories
    */
@@ -22,6 +22,7 @@ class Cassie(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) {
   val scalaTest = buildScalaVersion match {
     case "2.8.0.Beta1" => "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-with-test-interfaces-0.3-SNAPSHOT" % "test" withSources() intransitive()
     case "2.8.0.RC2" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC2-SNAPSHOT" % "test" withSources() intransitive()
+    case "2.8.0.RC3" => "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC3-SNAPSHOT" % "test" withSources() intransitive()
     case unknown => error("no known scalatest impl for %s".format(unknown))
   }
   val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test" withSources()
@@ -39,16 +40,9 @@ class Cassie(info: sbt.ProjectInfo) extends sbt.DefaultProject(info) {
   override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageSrc, `package`)
 
   /**
-   * Publish to a local dir, and then rsync with my repo.
+   * Publish via rsync.
    */
-  override def managedStyle = sbt.ManagedStyle.Maven
-  val publishTo = sbt.Resolver.file("Local Cache", ("." / "target" / "repo").asFile)
-  def publishToLocalRepoAction = super.publishAction
-  override def publishAction = task {
-    log.info("Uploading to repo.codahale.com")
-    sbt.Process("rsync", "-avz" :: "target/repo/" :: "codahale.com:/home/codahale/repo.codahale.com" :: Nil) ! log
-    None
-  } describedAs("Publish binary and source JARs to repo.codahale.com") dependsOn(test, publishToLocalRepoAction)
+  def rsyncRepo = "codahale.com:/home/codahale/repo.codahale.com"
 
   /**
    * Build a JAR file with class files for Cassie, Cassandra, and Thrift.
