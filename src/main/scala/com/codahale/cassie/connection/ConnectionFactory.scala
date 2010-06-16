003b8bfca2e964f5bc6073661a2cf1e6f9e3a4f7
Coda Hale <coda.hale@gmail.com>
2010-06-16 01:47:25 -0700

    Added ConnectionFactory.

---
+++ src/main/scala/com/codahale/cassie/connection/ConnectionFactory.scala
@@ -0,0 +1,27 @@
+package com.codahale.cassie.connection
+
+import org.apache.commons.pool.BasePoolableObjectFactory
+import com.codahale.logula.Logging
+
+/**
+ * A factory for `Connection` objects.
+ *
+ * @author coda
+ */
+class ConnectionFactory(factory: ClientFactory)
+        extends BasePoolableObjectFactory with Logging {
+  def makeObject = {
+    log.fine("Creating a new connection to %s", factory)
+    new Connection(factory)
+  }
+
+  override def validateObject(obj: Any) = obj match {
+    case conn: Connection => conn.isHealthy()
+    case _ => false
+  }
+
+  override def destroyObject(obj: Any) = obj match {
+    case conn: Connection => conn.close()
+    case _ =>
+  }
+}
