a8282ab561b6b964776ea2d319f75d0383a7a462
Coda Hale <coda.hale@gmail.com>
2010-06-17 18:22:48 -0700

    Removed a debugging println.

--- src/test/scala/com/codahale/cassie/FNV1ATest.scala
+++ src/test/scala/com/codahale/cassie/FNV1ATest.scala
@@ -6,7 +6,6 @@ import org.scalatest.matchers.MustMatchers
 class FNV1ATest extends Spec with MustMatchers {
   describe("the FNV1A hash function") {
     it("matches up with existing implementations") {
-      println("%x".format(FNV1A("foobar".getBytes)))
       FNV1A("foobar".getBytes) must equal(0x85944171f73967e8L)
     }
   }
