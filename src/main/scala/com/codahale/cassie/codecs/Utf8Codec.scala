097ed107856ae3d33b0f3479400f83f9a5b1ccf0
Coda Hale <coda.hale@gmail.com>
2010-06-22 08:06:06 -0700

    Use Thrift's new fast UTF8 decoder instead of String's constructor.

--- src/main/scala/com/codahale/cassie/codecs/Utf8Codec.scala
+++ src/main/scala/com/codahale/cassie/codecs/Utf8Codec.scala
@@ -1,8 +1,7 @@
 package com.codahale.cassie.codecs
 
 import java.lang.String
-import java.nio.charset.Charset
-import java.nio.ByteBuffer
+import org.apache.thrift.bootleg.Utf8Helper
 
 /**
  * Encodes and decodes values as UTF-8 strings.
@@ -10,8 +9,6 @@ import java.nio.ByteBuffer
  * @author coda
  */
 object Utf8Codec extends Codec[String] {
-  private val utf8 = Charset.forName("UTF-8")
-
-  def encode(obj: String) = obj.getBytes(utf8)
-  def decode(ary: Array[Byte]) = utf8.decode(ByteBuffer.wrap(ary)).toString
+  def encode(s: String) = Utf8Helper.encode(s)
+  def decode(ary: Array[Byte]) = Utf8Helper.decode(ary)
 }
