b5c44788171c7c9caff7ee559f3b09b8012df865
Stu Hood <stuhood@twitter.com>
2011-02-07 15:16:16 -0800

    Remove coda`s packaging magic: defaults appear to work swimmingly.

--- project/build/Cassie.scala
+++ project/build/Cassie.scala
@@ -37,35 +37,4 @@ class Cassie(info: sbt.ProjectInfo) extends StandardProject(info)
    * Build Options
    */
   override def compileOptions = Deprecation :: Unchecked :: super.compileOptions.toList
-
-  /**
-   * Include docs and source as build artifacts.
-   */
-  override def packageSrcJar = defaultJarPath("-sources.jar")
-  val sourceArtifact = sbt.Artifact(artifactID, "src", "jar", Some("sources"), Nil, None)
-  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageSrc, `package`)
-
-  /**
-   * Build a JAR file with class files for Cassie, Cassandra, and Thrift.
-   */
-  def assemblyExclude(base: sbt.PathFinder) = base / "META-INF" ** "*"
-  def assemblyOutputPath = jarPath
-  def assemblyTemporaryPath = outputPath / "assembly-libs"
-  def assemblyClasspath = fullUnmanagedClasspath(sbt.Configurations.Runtime)
-
-  def assemblyPaths(tempDir: sbt.Path, classpath: sbt.PathFinder, exclude: sbt.PathFinder => sbt.PathFinder) = {
-    val (libs, directories) = classpath.get.toList.partition(sbt.ClasspathUtilities.isArchive)
-    for(jar <- libs) {
-      val jarName = jar.asFile.getName
-      log.info("Including %s".format(jarName))
-      sbt.FileUtilities.unzip(jar, tempDir, log).left.foreach(error)
-    }
-    val base = (sbt.Path.lazyPathFinder(tempDir :: directories) ##)
-    (descendents(base, "*") --- exclude(base)).get
-  }
-
-  def assemblyTask(tempDir: sbt.Path, classpath: sbt.PathFinder, exclude: sbt.PathFinder => sbt.PathFinder) = {
-    packageTask(sbt.Path.lazyPathFinder(assemblyPaths(tempDir, classpath, exclude)), assemblyOutputPath, packageOptions)
-  }
-  override def packageAction = assemblyTask(assemblyTemporaryPath, assemblyClasspath, assemblyExclude).dependsOn(compile, copyResources) describedAs("Builds a JAR with Cassandra and Thrift classes included.")
 }
