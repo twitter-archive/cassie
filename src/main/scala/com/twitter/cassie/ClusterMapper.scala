a3784a6224ca518fdb895040747bb942623d6712
Ryan King <ryan@theryanking.com>
2011-02-07 17:57:37 -0800

    periodically remap

--- src/main/scala/com/twitter/cassie/ClusterMapper.scala
+++ src/main/scala/com/twitter/cassie/ClusterMapper.scala
@@ -8,6 +8,8 @@ import scala.collection.JavaConversions._
 import com.twitter.cassie.connection.ClusterClientProvider
 import com.twitter.finagle.builder.SocketAddressCluster
 import com.twitter.finagle.builder.{Cluster => FCluster}
+import com.twitter.admin.PeriodicBackgroundProcess
+import com.twitter.conversions.time._
 
 /**
  * Given a seed host and port, returns a set of nodes in the cluster.
@@ -21,20 +23,31 @@ import com.twitter.finagle.builder.{Cluster => FCluster}
  */
 private class ClusterMapper(keyspace: String, seedHost: String, seedPort: Int = 9160, timeoutMS: Int = 10000) extends FCluster {
   val log = Logger.get
-  private val ccp = new ClusterClientProvider(
-    new SocketAddressCluster(Seq(new InetSocketAddress(seedHost, seedPort))),
-    keyspace,
-    readTimeoutInMS = timeoutMS,
-    maxConnectionsPerHost = 1
-  )
 
-  def mapHosts[A](f: SocketAddress => A): Seq[A] = {
+  var hosts = fetchHosts
+
+  val refetcher =  new PeriodicBackgroundProcess("ClusterMapper", 1.minute) {
+    def periodic() {
+      hosts = fetchHosts
+    }
+  }
+
+  def fetchHosts = {
+     val ccp = new ClusterClientProvider(
+      new SocketAddressCluster(Seq(new InetSocketAddress(seedHost, seedPort))),
+      keyspace,
+      readTimeoutInMS = timeoutMS,
+      maxConnectionsPerHost = 1
+    )
     log.info("Mapping cluster...")
     val ring = ccp.map{ _.describe_ring(keyspace) }()
     ccp.close
     log.debug("Received: %s", ring)
-    val hosts = asScalaIterable(ring).flatMap{ h => asScalaIterable(h.endpoints).map{ host =>
+    asScalaIterable(ring).flatMap{ h => asScalaIterable(h.endpoints).map{ host =>
       new InetSocketAddress(host, seedPort) } }
+  }
+
+  def mapHosts[A](f: SocketAddress => A): Seq[A] = {
     hosts.toSeq map f
   }
 
