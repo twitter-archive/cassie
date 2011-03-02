dfd01e318ad7950f0311e9a1073a3b340aaf9ec3
Ian Ownbey <imownbey@gmail.com>
2011-03-01 17:22:27 -0800

    Added support for ttls

--- src/test/scala/com/twitter/cassie/examples/CassieRun.scala
+++ src/test/scala/com/twitter/cassie/examples/CassieRun.scala
@@ -3,6 +3,7 @@ package com.twitter.cassie.tests.examples
 import com.twitter.cassie._
 import clocks.MicrosecondEpochClock
 import com.twitter.logging.Logger
+import com.twitter.conversions.time._
 import org.apache.log4j.Level
 import types.{LexicalUUID, VarInt, AsciiString, FixedLong}
 // TODO: unfortunate
@@ -39,6 +40,8 @@ object CassieRun {
 
     cass.insert("yay for everyone", Column("name", "Louie")).apply()
     cass.insert("yay for everyone", Column("motto", "Swish!")).apply()
+
+    cass.insert("HELLO", Column("name", "awww yeahhhh", 1.minute)).apply()
 
     log.info("getting a column: %s", cass.getColumn("yay for me", "name").apply())
     // Some(Column(name,Coda,1271789761374109))
