72a7e46371b3b135e998c861656e59cb46d7de23
Ian Ownbey <imownbey@gmail.com>
2011-03-06 16:33:14 -0800

    Compile

---
+++ src/main/scala/com/twitter/cassie/CounterColumn.scala
@@ -0,0 +1,39 @@
+package com.twitter.cassie
+
+import clocks.Clock
+
+import codecs.Codec
+import org.apache.cassandra.thrift
+
+object CounterColumn {
+
+  private[cassie] def convert[A](nameCodec: Codec[A], counter: thrift.Counter): CounterColumn[A] = {
+    CounterColumn(
+      nameCodec.decode(counter.column.name),
+      counter.column.value
+    )
+  }
+
+  private[cassie] def convert[A](nameCodec: Codec[A], counter: thrift.CounterColumn): CounterColumn[A] = {
+    CounterColumn(
+      nameCodec.decode(counter.name),
+      counter.value
+    )
+  }
+
+  private[cassie] def convert[A](nameCodec: Codec[A], col: CounterColumn[A]): thrift.CounterColumn = {
+    new thrift.CounterColumn(
+      nameCodec.encode(col.name),
+      col.value
+    )
+  }
+}
+
+/**
+ * A column in a Cassandra. Belongs to a row in a column family.
+ *
+ * @author coda
+ */
+case class CounterColumn[A](name: A, value: Long) {
+  def pair = name -> this
+}
