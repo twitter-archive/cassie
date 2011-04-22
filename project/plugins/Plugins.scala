948d31cf103d5eecbd1f39f19fdbada3239bceca
Ryan King <ryan@theryanking.com>
2011-04-22 10:33:52 -0700

    allow building open source only

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,7 +1,20 @@
 import sbt._
 
 class Plugins(info: sbt.ProjectInfo) extends sbt.PluginDefinition(info) {
-  val twitterMaven = "twitter.com" at "http://maven.twttr.com/"
+   import scala.collection.jcl
+   val environment = jcl.Map(System.getenv())
+   def isSBTOpenTwitter = environment.get("SBT_OPEN_TWITTER").isDefined
+   def isSBTTwitter = environment.get("SBT_TWITTER").isDefined
+
+   override def repositories = if (isSBTOpenTwitter) {
+     Set("twitter.artifactory" at "http://artifactory.local.twitter.com/open-source/")
+   } else if (isSBTTwitter) {
+     Set("twitter.artifactory" at "http://artifactory.local.twitter.com/repo/")
+   } else {
+     super.repositories ++ Seq("twitter.com" at "http://maven.twttr.com/")
+   }
+   override def ivyRepositories = Seq(Resolver.defaultLocal(None)) ++ repositories
+
   val defaultProject = "com.twitter" % "standard-project" % "0.11.3"
   val sbtThrift = "com.twitter" % "sbt-thrift" % "1.1.0"
 
