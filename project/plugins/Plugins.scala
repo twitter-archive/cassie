58e86bb7a8f64518854162a3e02dd2d9d394cac4
Ryan King <ryan@theryanking.com>
2011-04-27 13:05:56 -0700

    remove unused repos

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -17,9 +17,4 @@ class Plugins(info: sbt.ProjectInfo) extends sbt.PluginDefinition(info) {
 
   val defaultProject = "com.twitter" % "standard-project" % "0.11.3"
   val sbtThrift = "com.twitter" % "sbt-thrift" % "1.1.0"
-
-  val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
-  val posterous = "net.databinder" % "posterous-sbt" % "0.1.4"
-
-  val codasRepo = "codahale.com" at "http://repo.codahale.com/"
 }
