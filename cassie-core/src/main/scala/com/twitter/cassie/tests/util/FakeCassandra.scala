39e3cdc25f47a261138ab98f4ebb1d2e93ff7a19
Kyle Maxwell <kmaxwell@twitter.com>
2011-04-28 16:07:09 -0700

    support more operations

--- cassie-core/src/main/scala/com/twitter/cassie/tests/util/FakeCassandra.scala
+++ cassie-core/src/main/scala/com/twitter/cassie/tests/util/FakeCassandra.scala
@@ -7,6 +7,7 @@ import java.util.concurrent.CountDownLatch
 import org.apache.cassandra.finagle.thrift._
 import java.nio.ByteBuffer
 import java.util._
+import com.twitter.cassie.codecs.Utf8Codec
 import scala.collection.JavaConversions._
 
 object FakeCassandra {
@@ -32,46 +33,47 @@ object FakeCassandra {
  * support a limited and expanding subset of the cassandra api.
  */
 class FakeCassandra(val port: Int) extends Cassandra.Iface {
-    // Taken from cassandra ByteBufferUtil#compareUnsigned
-    // Questionable style because it's as straight a port as possible
-    val comparator = new Comparator[ByteBuffer] {
-      def compare(o1: ByteBuffer, o2: ByteBuffer): Int = {
-        if(null == o1) {
-          if(null == o2) return 0
-          else return -1
-        }
-
-        val minLength = Math.min(o1.remaining(), o2.remaining())
-        var x = 0
-        var i = o1.position()
-        var j = o2.position()
-        while (x < minLength) {
-          if (o1.get(i) != o2.get(j)) {
-
-            // compare non-equal bytes as unsigned
-            return if ((o1.get(i) & 0xFF) < (o2.get(j) & 0xFF)) -1 else 1
-          }
-
-          x += 1
-          i += 1
-          j += 1
-        }
-
-        return if (o1.remaining() == o2.remaining()) 0 else (if (o1.remaining() < o2.remaining()) -1 else 1)
+  // Taken from cassandra ByteBufferUtil#compareUnsigned
+  // Questionable style because it's as straight a port as possible
+  val comparator = new Comparator[ByteBuffer] {
+    def compare(o1: ByteBuffer, o2: ByteBuffer): Int = {
+      if(null == o1) {
+        if(null == o2) return 0
+        else return -1
       }
+
+      val minLength = Math.min(o1.remaining(), o2.remaining())
+      var x = 0
+      var i = o1.position()
+      var j = o2.position()
+      while (x < minLength) {
+        if (o1.get(i) != o2.get(j)) {
+
+          // compare non-equal bytes as unsigned
+          return if ((o1.get(i) & 0xFF) < (o2.get(j) & 0xFF)) -1 else 1
+        }
+
+        x += 1
+        i += 1
+        j += 1
+      }
+
+      return if (o1.remaining() == o2.remaining()) 0 else (if (o1.remaining() < o2.remaining()) -1 else 1)
+    }
   }
 
   val columnComparator = new Comparator[Column] {
     def compare(a: Column, b: Column) = comparator.compare(a.BufferForName, b.BufferForName)
   }
 
-  val thread = new FakeCassandra.ServerThread(this, port)
+  var thread: FakeCassandra.ServerThread = null 
   var currentKeyspace = "default"
 
   //                     keyspace        CF              row         column
   val data = new TreeMap[String, TreeMap[String, TreeMap[ByteBuffer, SortedSet[Column]]]]
 
   def start() = {
+    thread = new FakeCassandra.ServerThread(this, port)
     thread.start()
     thread.latch.await()
   }
@@ -91,7 +93,12 @@ class FakeCassandra(val port: Int) extends Cassandra.Iface {
     cf
   }
 
-  def stop() = thread.server.stop()
+  def stop() = {
+    thread.server.stop()
+    reset()
+  }
+  
+  def reset() = data.clear()
 
   def set_keyspace(keyspace: String) = currentKeyspace = keyspace
 
@@ -105,7 +112,9 @@ class FakeCassandra(val port: Int) extends Cassandra.Iface {
     row.add(column)
   }
 
-  def get_slice(key: ByteBuffer, column_parent: ColumnParent, predicate: SlicePredicate, consistency_level: ConsistencyLevel) = {
+  def get_slice(key: ByteBuffer, column_parent: ColumnParent, predicate: SlicePredicate, consistency_level: ConsistencyLevel): List[ColumnOrSuperColumn] = get_slice(key, column_parent, predicate, consistency_level, System.currentTimeMillis, false)
+  
+  def get_slice(key: ByteBuffer, column_parent: ColumnParent, predicate: SlicePredicate, consistency_level: ConsistencyLevel, asOf: Long, andDelete: Boolean): List[ColumnOrSuperColumn] = {
     val cf = getColumnFamily(column_parent)
     var row = cf.get(key)
     val list = new ArrayList[ColumnOrSuperColumn]
@@ -115,37 +124,74 @@ class FakeCassandra(val port: Int) extends Cassandra.Iface {
       list.add(cosc)
     }
     if (row != null) {
-      if (predicate.isSetColumn_names()) {
-        throw new UnsupportedOperationException
-      } else if (predicate.isSetSlice_range()) {
-        val sr = predicate.getSlice_range
+      var limit = Int.MaxValue
+      val rowView: SortedSet[Column] = if (predicate.isSetSlice_range()) {
         val start = new Column
         val finish = new Column
+        val sr = predicate.getSlice_range
         start.setName(if(sr.isSetStart) sr.getStart else Array.empty[Byte])
+        
+        if (sr.isSetCount && sr.getCount > 0) limit = sr.getCount
         if(sr.isSetFinish && sr.getFinish.length > 0) {
           finish.setName(sr.getFinish)
-          row = row.subSet(start, finish)
+          row.subSet(start, finish)
         } else {
-          row = row.tailSet(start)
-        }
-
-        var limit = if (sr.isSetCount && sr.getCount > 0) sr.getCount else Int.MaxValue
-        var i = 0
-        for(entry <- row) {
-          if(i < limit) {
-            add(entry)
-            i += 1
-          }
+          row.tailSet(start)
         }
       } else {
-        for(entry <- row) {
-          add(entry)
+        row
+      }
+      
+      val names = new HashSet[String]
+      if (predicate.isSetColumn_names) {
+        for(name <- predicate.getColumn_names) names.add(Utf8Codec.decode(name))
+      }
+      
+      var i = 0
+      val toRemove = new ArrayList[Column]
+      for(entry <- rowView) {
+        if(i < limit &&
+          (names.isEmpty || names.contains(Utf8Codec.decode(ByteBuffer.wrap(entry.getName))))
+          // && entry.getTimestamp <= asOf
+          ) {
+          if(andDelete) {
+
+            toRemove.add(entry)
+          } else {
+            add(entry)
+          }
+          i += 1
         }
+      }
+      for(entry <- toRemove) {
+        row.remove(entry)
       }
     }
     list
   }
-
+  def batch_mutate(mutation_Map: Map[ByteBuffer,Map[String,List[Mutation]]], consistency_level: ConsistencyLevel) = {
+    for((key, map) <- mutation_Map) {
+      for((cf, mutations) <- map) {
+        val cp = new ColumnParent
+        cp.setColumn_family(cf)
+        for(mutation <- mutations) {
+          if(mutation.isSetColumn_or_supercolumn) {
+            val cosc = mutation.getColumn_or_supercolumn
+            if(cosc.isSetColumn) {
+              insert(key, cp, cosc.getColumn, ConsistencyLevel.ANY)
+            } else {
+              throw new UnsupportedOperationException("no supercolumn support")
+            }
+          }
+          if(mutation.isSetDeletion) {
+            val deletion = mutation.getDeletion
+            val time = if (deletion.isSetTimestamp) deletion.getTimestamp else System.currentTimeMillis
+            get_slice(key, cp, deletion.getPredicate, ConsistencyLevel.ANY, time, true)
+          }
+        }
+      }
+    }
+  }
 
   def login(auth_request: AuthenticationRequest ) = throw new UnsupportedOperationException
   def get(key: ByteBuffer, column_path: ColumnPath, consistency_level: ConsistencyLevel) = throw new UnsupportedOperationException
@@ -156,7 +202,6 @@ class FakeCassandra(val port: Int) extends Cassandra.Iface {
   def get_indexed_slices(column_parent: ColumnParent, index_clause: IndexClause, column_predicate: SlicePredicate, consistency_level: ConsistencyLevel) = throw new UnsupportedOperationException
 
   def remove(key: ByteBuffer, column_path: ColumnPath, timestamp: Long, consistency_level: ConsistencyLevel) = throw new UnsupportedOperationException
-  def batch_mutate(mutation_Map: Map[ByteBuffer,Map[String,List[Mutation]]], consistency_level: ConsistencyLevel) = throw new UnsupportedOperationException
   def truncate(cfname: String) = throw new UnsupportedOperationException
   def add(key: ByteBuffer, column_parent: ColumnParent, column: CounterColumn, consistency_level: ConsistencyLevel) = throw new UnsupportedOperationException
   def batch_add(update_Map: Map[ByteBuffer,Map[String,List[CounterMutation]]], consistency_level: ConsistencyLevel) = throw new UnsupportedOperationException
--- cassie-core/src/test/scala/com/twitter/cassie/util/FakeCassandraTest.scala
+++ cassie-core/src/test/scala/com/twitter/cassie/util/FakeCassandraTest.scala
@@ -2,7 +2,7 @@ package com.twitter.cassie.tests.util
 
 import org.scalatest.matchers.MustMatchers
 import org.mockito.Mockito.when
-import org.scalatest.{BeforeAndAfterAll, Spec}
+import org.scalatest._
 import java.net.{SocketAddress, InetSocketAddress}
 import org.apache.cassandra.finagle.thrift
 
@@ -14,7 +14,7 @@ import com.twitter.conversions.time._
 import scala.collection.mutable.ListBuffer
 import com.twitter.cassie.connection.CCluster
 
-class FakeCassandraTest extends Spec with MustMatchers with BeforeAndAfterAll {
+class FakeCassandraTest extends Spec with MustMatchers with BeforeAndAfterAll with BeforeAndAfter {
   val port = 1359
   def factory() = new FakeCassandra(port)
   var server: FakeCassandra = null
@@ -41,11 +41,36 @@ class FakeCassandraTest extends Spec with MustMatchers with BeforeAndAfterAll {
     connections.map(_.close)
   }
 
+  override protected def beforeEach() {
+    server.reset()
+  }
+
   describe("a fake cassandra") {
     it("should be able to connect to an arbitrary columnfamily and read and write") {
       val cf = keyspace().columnFamily[String, String, String]("bar")
       cf.insert("k", Column("b", "c")).get()
-      cf.getRow("k").get().size() must equal(1)
+      cf.getRow("k").get().size must equal(1)
+      println("a")
+      cf.getColumn("k", "b").get().size must equal(1)
+      println("b")
+    }
+
+    it("should be able to batch mutate") {
+      val cf = keyspace().columnFamily[String, String, String]("bar")
+      var batch = cf.batch
+      batch.insert("k", Column("b", "c"))
+      batch.insert("k", Column("d", "e"))
+      batch.execute().get()
+      cf.getRow("k").get().size() must equal(2)
+      
+      batch = cf.batch
+      batch.removeColumn("k", "b")
+      batch.removeColumn("k", "d")
+      println("rm")
+      batch.execute().get()
+            println("rm")
+      cf.getRow("k").get().size() must equal(0)
+      
     }
   }
 }
