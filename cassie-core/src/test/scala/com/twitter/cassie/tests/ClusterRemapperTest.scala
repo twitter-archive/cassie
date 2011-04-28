cfe4cfd37a4f2702312da459964e47b1a0037d9c
Ryan King <ryan@theryanking.com>
2011-04-28 13:37:10 -0700

    get rid of disabled test for now

--- cassie-core/src/test/scala/com/twitter/cassie/tests/ClusterRemapperTest.scala
+++ cassie-core/src/test/scala/com/twitter/cassie/tests/ClusterRemapperTest.scala
@@ -13,25 +13,25 @@ import scala.collection.JavaConversions._
 import com.twitter.conversions.time._
 
 class ClusterRemapperTest extends Spec with MustMatchers with BeforeAndAfterAll {
-  val server = new MockCassandraServer(MockCassandraServer.choosePort())
-  val ring = tr("start", "end", "c1.example.com") ::
-    tr("start", "end", "c2.example.com") :: Nil
-  when(server.cassandra.describe_ring("keyspace")).thenReturn(asJavaList(ring))
-
-  def tr(start: String, end: String, endpoints: String*): thrift.TokenRange = {
-    val tr = new thrift.TokenRange()
-    tr.setStart_token(start)
-    tr.setEnd_token(end)
-    tr.setEndpoints(asJavaList(endpoints))
-  }
-
-  override protected def beforeAll() {
-    server.start()
-  }
-
-  override protected def afterAll() {
-    server.stop()
-  }
+  // val server = new MockCassandraServer(MockCassandraServer.choosePort())
+  // val ring = tr("start", "end", "c1.example.com") ::
+  //   tr("start", "end", "c2.example.com") :: Nil
+  // when(server.cassandra.describe_ring("keyspace")).thenReturn(asJavaList(ring))
+  // 
+  // def tr(start: String, end: String, endpoints: String*): thrift.TokenRange = {
+  //   val tr = new thrift.TokenRange()
+  //   tr.setStart_token(start)
+  //   tr.setEnd_token(end)
+  //   tr.setEndpoints(asJavaList(endpoints))
+  // }
+  // 
+  // override protected def beforeAll() {
+  //   server.start()
+  // }
+  // 
+  // override protected def afterAll() {
+  //   server.stop()
+  // }
 
   // describe("mapping a cluster") {
   //   it("returns the set of nodes in the cluster") {
@@ -44,6 +44,6 @@ class ClusterRemapperTest extends Spec with MustMatchers with BeforeAndAfterAll 
   //     ))
   //   }
   // }
-
-  def addr(host: String, port: Int) = new InetSocketAddress(host, port)
+  // 
+  // def addr(host: String, port: Int) = new InetSocketAddress(host, port)
 }
