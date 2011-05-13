2878ad5234931dbf1520eee03ea7cc6b8f91e5ce
Ryan King <ryan@theryanking.com>
2011-05-12 17:39:12 -0700

    add test file

---
+++ test.scala
@@ -0,0 +1,12 @@
+import com.twitter.ostrich.admin.RuntimeEnvironment
+import com.twitter.ostrich.admin.AdminHttpService
+import com.twitter.conversions.time._
+import com.twitter.cassie._
+import types.{LexicalUUID, VarInt, AsciiString, FixedLong}
+import com.twitter.cassie.Codecs._
+
+var a = new AdminHttpService(9990, 10, new RuntimeEnvironment(this))
+a.start
+val cluster = new Cluster("127.0.0.1")
+val keyspace = cluster.keyspace("test").connect();
+val cf = keyspace.columnFamily[String, String, String]("test")
