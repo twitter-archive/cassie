53290fb2e4117246ad09fd672ed89204041e600a
Ian Ownbey <ian@twitter.com>
2011-06-15 18:10:22 -0700

    Allow iterating over columns

--- cassie-core/src/main/scala/com/twitter/cassie/ColumnFamily.scala
+++ cassie-core/src/main/scala/com/twitter/cassie/ColumnFamily.scala
@@ -229,6 +229,14 @@ case class ColumnFamily[Key, Name, Value](
     new ColumnIteratee(this, EMPTY, EMPTY, batchSize, pred, keyCodec, nameCodec, valueCodec)
   }
 
+  private[cassie] def columnsIterateeAs[K, N, V](key: K, batchSize: Int)
+      (implicit keyCodec: Codec[K], nameCodec: Codec[N], valueCodec: Codec[V]): ColumnIteratee[K, N, V] = {
+    new ColumnIteratee(this, keyCodec.encode(key), keyCodec.encode(key), batchSize, new thrift.SlicePredicate, keyCodec, nameCodec, valueCodec)
+  }
+
+  def columnsIteratee(key: Key, batchSize: Int): ColumnIteratee[Key, Name, Value] =
+    columnsIterateeAs[Key, Name, Value](key, batchSize)(defaultKeyCodec, defaultNameCodec, defaultValueCodec)
+
   def columnsIteratee(batchSize: Int,
                       columnNames: Set[Name]): ColumnIteratee[Key, Name, Value] = {
     columnsIterateeAs[Key, Name, Value](batchSize, columnNames)(defaultKeyCodec, defaultNameCodec, defaultValueCodec)
