31cb918e2ce5d70090b6f672cbf4d76e66eeec93
Ryan King <ryan@theryanking.com>
2011-06-23 11:26:12 -0700

    try upgrading util version to help out rvp

--- project/build/Cassie.scala
+++ project/build/Cassie.scala
@@ -35,7 +35,7 @@ class Cassie(info: sbt.ProjectInfo) extends StandardParentProject(info)
     val finagle = "com.twitter" % "finagle-core" % finagleVersion
     val finagleThrift = "com.twitter" % "finagle-thrift" % finagleVersion
     val finagleOstrich = "com.twitter" % "finagle-ostrich4" % finagleVersion
-    val utilCore = "com.twitter" % "util-core" % "1.8.12"
+    val utilCore = "com.twitter" % "util-core" % "1.8.18"
 
     val slf4jNop = "org.slf4j" %  "slf4j-nop" % slf4jVersion % "provided"
 
