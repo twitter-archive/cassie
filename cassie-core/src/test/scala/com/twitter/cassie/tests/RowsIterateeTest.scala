555b875c95251ca0f8c65ebbef7439933c2c067f
Ryan King <ryan@twitter.com>
2011-07-26 18:20:27 -0700

    some test cleanup

--- cassie-core/src/test/scala/com/twitter/cassie/tests/RowsIterateeTest.scala
+++ cassie-core/src/test/scala/com/twitter/cassie/tests/RowsIterateeTest.scala
@@ -19,21 +19,21 @@ import com.twitter.cassie.util.ColumnFamilyTestHelper
 
 class RowsIterateeTest extends Spec with MustMatchers with MockitoSugar with OneInstancePerTest with ColumnFamilyTestHelper{
 
-  def newCf() = {
-    val mcc = new MockCassandraClient
-    val cf = new ColumnFamily("ks", "People", new SimpleProvider(mcc.client),
-          Utf8Codec, Utf8Codec, Utf8Codec,
-          ReadConsistency.Quorum, WriteConsistency.Quorum)
-    (mcc, cf)
-  }
-
-  def c(name: String, value: String, timestamp: Long) = {
+  def co(name: String, value: String, timestamp: Long) = {
     new Column(name, value, Some(timestamp), None)
   }
-  def anyByteBuffer = any(classOf[ByteBuffer])
-  def anyString = any(classOf[String])
-  def anyInt = any(classOf[Int])
-  def b(string: String) = ByteBuffer.wrap(string.getBytes)
+
+  def keyRange(start: String, end:String, count:Int) = {
+    new thrift.KeyRange().setStart_key(b(start)).setEnd_key(b(end)).setCount(count)
+  }
+
+  def keySlice(cf: ColumnFamily[String, String, String], key: String, columns: Seq[Column[String, String]]) = {
+    new thrift.KeySlice()
+      .setKey(b(key))
+      .setColumns(
+        asJavaList(columns.map(c => new thrift.ColumnOrSuperColumn().setColumn(Column.convert(cf.nameCodec, cf.valueCodec, cf.clock, c))))
+      )
+  }
 
   describe("iterating through an empty column family") {
     val (client, cf) = setup
@@ -50,25 +50,17 @@ class RowsIterateeTest extends Spec with MustMatchers with MockitoSugar with One
     }
   }
 
-  def keySlice(cf: ColumnFamily[String, String, String], key: String, columns: Seq[Column[String, String]]) = {
-    new thrift.KeySlice()
-      .setKey(b(key))
-      .setColumns(
-        asJavaList(columns.map(c => new thrift.ColumnOrSuperColumn().setColumn(Column.convert(cf.nameCodec, cf.valueCodec, cf.clock, c))))
-      )
-  }
-
   describe("iterating through the columns of a range of keys") {
     val (client, cf) = setup
 
     when(client.get_range_slices(any(classOf[thrift.ColumnParent]), any(classOf[thrift.SlicePredicate]) , any(classOf[thrift.KeyRange]), any(classOf[thrift.ConsistencyLevel]))).thenReturn(
       Future.value(
         asJavaList(List(
-          keySlice(cf, "start",  List(c("name", "value", 1), c("name1", "value1", 2))),
-          keySlice(cf, "start1", List(c("name", "value", 1), c("name1", "value1", 2))),
-          keySlice(cf, "start2", List(c("name", "value", 1), c("name1", "value1", 2))),
-          keySlice(cf, "start3", List(c("name", "value", 1), c("name1", "value1", 2)))))),
-      Future.value(asJavaList(List(keySlice(cf, "start3", List(c("name", "value", 1), c("name1", "value1", 2))))))
+          keySlice(cf, "start",  List(co("name", "value", 1), co("name1", "value1", 2))),
+          keySlice(cf, "start1", List(co("name", "value", 1), co("name1", "value1", 2))),
+          keySlice(cf, "start2", List(co("name", "value", 1), co("name1", "value1", 2))),
+          keySlice(cf, "start3", List(co("name", "value", 1), co("name1", "value1", 2)))))),
+      Future.value(asJavaList(List(keySlice(cf, "start3", List(co("name", "value", 1), co("name1", "value1", 2))))))
     )
 
     val iterator = cf.rowsIteratee("start", "end", 5, new JHashSet())
@@ -81,24 +73,20 @@ class RowsIterateeTest extends Spec with MustMatchers with MockitoSugar with One
 
     it("does a buffered iteration over the columns in the rows in the range") {
       data must equal(ListBuffer(
-        ("start",  asJavaList(List(c("name", "value", 1), c("name1", "value1", 2)))),
-        ("start1", asJavaList(List(c("name", "value", 1), c("name1", "value1", 2)))),
-        ("start2", asJavaList(List(c("name", "value", 1), c("name1", "value1", 2)))),
-        ("start3", asJavaList(List(c("name", "value", 1), c("name1", "value1", 2))))
+        ("start",  asJavaList(List(co("name", "value", 1), co("name1", "value1", 2)))),
+        ("start1", asJavaList(List(co("name", "value", 1), co("name1", "value1", 2)))),
+        ("start2", asJavaList(List(co("name", "value", 1), co("name1", "value1", 2)))),
+        ("start3", asJavaList(List(co("name", "value", 1), co("name1", "value1", 2))))
       ))
-    }
-
-    def keyRange(start: String, end:String, count:Int) = {
-      new thrift.KeyRange().setStart_key(b(start)).setEnd_key(b(end)).setCount(count)
     }
 
     it("requests data using the last key as the start key until the end is detected") {
       val f = iterator.foreach { case(key, columns) => () }
       f()
       val cp = new thrift.ColumnParent(cf.name)
-      val inOrder = inOrderVerify(mcc.client)
-      inOrder.verify(mcc.client).get_range_slices(matchEq(cp), any(classOf[thrift.SlicePredicate]), matchEq(keyRange("start", "end", 5)), any(classOf[thrift.ConsistencyLevel]))
-      inOrder.verify(mcc.client).get_range_slices(matchEq(cp), any(classOf[thrift.SlicePredicate]), matchEq(keyRange("start3", "end", 6)), any(classOf[thrift.ConsistencyLevel]))
+      val inOrder = inOrderVerify(client)
+      inOrder.verify(client).get_range_slices(matchEq(cp), any(classOf[thrift.SlicePredicate]), matchEq(keyRange("start", "end", 5)), any(classOf[thrift.ConsistencyLevel]))
+      inOrder.verify(client).get_range_slices(matchEq(cp), any(classOf[thrift.SlicePredicate]), matchEq(keyRange("start3", "end", 6)), any(classOf[thrift.ConsistencyLevel]))
     }
   }
 }
