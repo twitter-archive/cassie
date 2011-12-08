cd88f055f06520e791447282fef470a9adeda522
Stu Hood <stuhood@twitter.com>
2011-12-08 11:16:28 -0800

    [split] Squashed commit of the following:
    commit 8a2c5203bbb65b0111be72fa465237d3e6e8ef6c
    Author: Stu Hood <stuhood@twitter.com>
    Date:   Wed Dec 7 22:51:35 2011 -0800
    
        Move from RetryingFilter+TimeoutFilter to RetryPolicy

--- cassie-core/src/main/scala/com/twitter/cassie/connection/ClusterClientProvider.scala
+++ cassie-core/src/main/scala/com/twitter/cassie/connection/ClusterClientProvider.scala
@@ -2,21 +2,21 @@ package com.twitter.cassie.connection
 
 import java.net.InetSocketAddress
 import java.util.concurrent.TimeUnit
+
 import org.apache.cassandra.finagle.thrift.Cassandra.ServiceToClient
-import org.apache.thrift.protocol.TBinaryProtocol
 import org.apache.cassandra.finagle.thrift.{UnavailableException, TimedOutException}
-import com.twitter.finagle.Service
-import com.twitter.finagle.builder.ClientBuilder
-import com.twitter.finagle.thrift.{ThriftClientRequest, ThriftClientFramedCodec}
-import com.twitter.util.Duration
-import com.twitter.util.{Future, Throw, Timer, TimerTask, Time}
-import com.twitter.finagle.stats.{StatsReceiver, NullStatsReceiver}
-import com.twitter.finagle.{CodecFactory, Codec, ClientCodecConfig}
-import com.twitter.finagle.tracing.{Tracer, NullTracer}
 import org.apache.thrift.protocol.{TBinaryProtocol, TProtocolFactory}
 
-import com.twitter.finagle.service.{RetryingFilter, Backoff, TimeoutFilter}
 import com.twitter.finagle.{WriteException, RequestTimeoutException, ChannelException}
+import com.twitter.finagle.builder.ClientBuilder
+import com.twitter.finagle.service.{Backoff, RetryPolicy => FinagleRetryPolicy}
+import com.twitter.finagle.Service
+import com.twitter.finagle.stats.{StatsReceiver, NullStatsReceiver}
+import com.twitter.finagle.thrift.{ThriftClientRequest, ThriftClientFramedCodec}
+import com.twitter.finagle.{CodecFactory, Codec, ClientCodecConfig}
+import com.twitter.finagle.tracing.{Tracer, NullTracer}
+import com.twitter.util.Duration
+import com.twitter.util.{Future, Throw, Timer, TimerTask, Time, Try}
 
 sealed case class RetryPolicy()
 
@@ -44,52 +44,39 @@ private[cassie] class ClusterClientProvider(val hosts: CCluster,
     def stop() { throw new Exception("illegal use!") }
   }
 
-  private val idempotentRetryFilter = RetryingFilter[ThriftClientRequest, Array[Byte]](Backoff.const(Duration(0, TimeUnit.MILLISECONDS)) take (retries), statsReceiver) {
-    case Throw(ex: WriteException) => {
-      statsReceiver.counter("WriteException").incr
-      true
-    }
-    case Throw(ex: RequestTimeoutException) => {
-      statsReceiver.counter("RequestTimeoutException").incr
-      true
-    }
-    case Throw(ex: ChannelException) => {
-      statsReceiver.counter("ChannelException").incr
-      true
-    }
-    case Throw(ex: UnavailableException) => {
-      statsReceiver.counter("UnavailableException").incr
-      true
-    }
-    case Throw(ex: TimedOutException) => {
-      statsReceiver.counter("TimedOutException").incr
-      true
-    }
+  /** Record the given exception, and return true. */
+  private def recordRetryable(e: Exception): Boolean = {
+    statsReceiver.counter(e.getClass.getSimpleName()).incr
+    true
   }
 
-  private val nonIdempotentRetryFilter = RetryingFilter[ThriftClientRequest, Array[Byte]](Backoff.const(Duration(0, TimeUnit.MILLISECONDS)) take (retries), statsReceiver) {
-    case Throw(ex: WriteException) => {
-      statsReceiver.counter("WriteException").incr
-      true
-    }
-    case Throw(ex: UnavailableException) => {
-      statsReceiver.counter("UnavailableException").incr
-      true
-    }
+  val finagleRetryPolicy: FinagleRetryPolicy[Try[Nothing]] = retryPolicy match {
+    case RetryPolicy.Idempotent =>
+      FinagleRetryPolicy.backoff(Backoff.const(Duration(0, TimeUnit.MILLISECONDS)) take (retries)) {
+        case Throw(x: WriteException) => recordRetryable(x)
+        case Throw(x: RequestTimeoutException) => recordRetryable(x)
+        case Throw(x: ChannelException) => recordRetryable(x)
+        case Throw(x: UnavailableException) => recordRetryable(x)
+        // TODO: see http://jira.local.twitter.com/browse/CORESTORAGE-494: if this is a legit
+        // serverside timeout, then we should be careful about retrying, since the serverside
+        // timeout is ideally set to just a smidgeon below our client timeout, and we would
+        // thus wait a lot of extra time
+        case Throw(x: TimedOutException) => recordRetryable(x)
+        // TODO: do we need to retry IndividualRequestTimeoutException?
+      }
+    case RetryPolicy.NonIdempotent =>
+      FinagleRetryPolicy.backoff(Backoff.const(Duration(0, TimeUnit.MILLISECONDS)) take (retries)) {
+        case Throw(x: WriteException) => recordRetryable(x)
+        case Throw(x: UnavailableException) => recordRetryable(x)
+      }
   }
-
-  val retryFilter = retryPolicy match {
-    case RetryPolicy.Idempotent => idempotentRetryFilter
-    case RetryPolicy.NonIdempotent => nonIdempotentRetryFilter
-  }
-
-  val timeoutFilter = new TimeoutFilter[ThriftClientRequest, Array[Byte]](timeout)
 
   private var service = ClientBuilder()
       .cluster(hosts)
       .name("cassie")
       .codec(CassandraThriftFramedCodec())
-      // We don' use timeout here, because we add our out TimeoutFilter below.
+      .retryPolicy(finagleRetryPolicy)
+      .timeout(timeout)
       .requestTimeout(requestTimeout)
       .connectTimeout(connectTimeout)
       .hostConnectionCoresize(minConnectionsPerHost)
@@ -98,8 +85,6 @@ private[cassie] class ClusterClientProvider(val hosts: CCluster,
       .tracerFactory(tracerFactory)
       .hostConnectionMaxWaiters(hostConnectionMaxWaiters)
       .build()
-
-  service = timeoutFilter andThen retryFilter andThen service
 
   private val client = new ServiceToClient(service, new TBinaryProtocol.Factory())
 
